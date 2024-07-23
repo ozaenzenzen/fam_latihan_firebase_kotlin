06924fb702fbd9eb0d3e6058bd08a9f1edf089ea
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-23 18:34:44 +0700

    fcm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.config)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
